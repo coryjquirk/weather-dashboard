63dd9d821cb69c8a73be178776792ff800bc605c
Cory Quirk <coryjquirk@gmail.com>
2020-10-05 00:26:50 -0500

    auto capitalizes cities

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//date generator
 function GetClock(){
   var d=new Date();
   var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
@@ -6,7 +5,6 @@ function GetClock(){
   var clocktext=""+(nmonth+1)+"/"+ndate+"/"+nyear+"";
   document.getElementById('clockbox').innerHTML=clocktext;
   }
-
 
 function renderWeather(){
   GetClock();
@@ -16,11 +14,8 @@ function renderWeather(){
   function getCity() {
     var city = document.querySelector("#citysearch").value;
     console.log(city);
-    document.querySelector(".last").innerHTML = city;
-    document.querySelector("#bigcity").innerHTML = city + " ";
     return city;
   }
-
 
   //my APIKey from my openweather account
   var APIKey = "9ba884c10ca90d0ca1f8f0ec657c12b7"
@@ -30,12 +25,6 @@ function renderWeather(){
   //generic openweather API call looks like this:
   //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-
-
-
-
-
-
   //1dayforecast
   // Here we run our AJAX call to the OpenWeatherMap API
   $.ajax({
@@ -44,24 +33,19 @@ function renderWeather(){
     })
   // We store all of the retrieved data inside of an object called "response"
   .then(function(response) {
-
       // Log the queryURL
       console.log("current weather: " + queryURL1);
       // Log the resulting object
       console.log(response);
-
       // Convert the temp K to fahrenheit
       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
       // add temp content to html
       $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
       // Transfer content to HTML
       $(".city").html("<h1>" + response.name + " Weather Details</h1>");
       $(".tempF").text("Temperature: " + tempF.toFixed(2) + " °F");
       $(".humidity").text("Humidity: " + response.main.humidity + "%");
       $(".wind").text("Wind Speed: " + (response.wind.speed*2.236936).toFixed(1) + " MPH"); //converts m/sec to mph
-
       //getting lat and long for UV
       var lon = response.coord.lon;
       var lat = response.coord.lat;
@@ -85,6 +69,12 @@ function renderWeather(){
             }
           }
       });
+      document.querySelector("#last1").innerHTML = response.name;
+      document.querySelector("#bigcity").innerHTML = response.name + " ";
+      //from here, start putting each city search into local storage
+      //use a for loop to plug them into #last1, #last2, etc.
+      //have the #last1, #last2 retrieve cities from local storage with a delay of 1
+      //lastly, make each old city clickable
   });
 
   //5 DAY FORECAST
@@ -96,12 +86,10 @@ function renderWeather(){
   })
   // We store all of the retrieved data inside of an object called "response"
   .then(function(response) {
-
     // Log the queryURL
     console.log("forecast weather: " + queryURL2);
     // Log the resulting object
     console.log(response);
-
     //pull temp & convert from K to F
     function pullTemp(listIndex) {
       tempF = (response.list[listIndex].main.temp - 273.15) *1.80 + 32;
@@ -120,7 +108,6 @@ function renderWeather(){
         if (digitOne='0'){
           return digitTwo;
         }
-        
       }
       var yyyy = dateFlipper.substr(0, 4);
       return mm+"/"+removeZero()+"/"+yyyy;
@@ -145,22 +132,8 @@ function renderWeather(){
       $(dateid).text("Humidity: " + response.list[5+8*i].main.humidity + "%");
     }
   });
-
-
-
-
-
-  // if (cityBox.textContent = "") {
-  //   $(".citybox").css({"display": "none"});
-  // }
-
-
-
-
-
-
-
-  //
-  // use this for weather icon images
-  // var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 }
+
+
+// use this for weather icon images
+// var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
