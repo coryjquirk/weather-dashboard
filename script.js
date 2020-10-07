5784e6c85bd08015f91742c0c891a9a5eb3068b2
Cory Quirk <coryjquirk@gmail.com>
2020-10-06 21:07:00 -0500

    color scheme,mobile responsiveness

--- script.js
+++ script.js
@@ -1,52 +1,32 @@
+function getCity() {
+  var city = document.querySelector("#citysearch").value;
+  return city;
+}
+GetClock();
 function GetClock(){
   var d=new Date();
   var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
-  
   var clocktext=""+(nmonth+1)+"/"+ndate+"/"+nyear+"";
   document.getElementById('clockbox').innerHTML=clocktext;
-  }
-
+}
 function renderWeather(){
-  GetClock();
   setInterval(GetClock,1000);
-
-  getCity();
-  function getCity() {
-    var city = document.querySelector("#citysearch").value;
-    console.log(city);
-    return city;
-  }
-
-  //my APIKey from my openweather account
   var APIKey = "9ba884c10ca90d0ca1f8f0ec657c12b7"
   var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + getCity() + "&appid=" + APIKey;
   var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + getCity() + "&appid=" + APIKey;
-
-  //generic openweather API call looks like this:
-  //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-  //1dayforecast
-  // Here we run our AJAX call to the OpenWeatherMap API
+  
   $.ajax({
       url: queryURL1,
       method: "GET"
     })
   // We store all of the retrieved data inside of an object called "response"
   .then(function(response) {
-      // Log the queryURL
-      console.log("current weather: " + queryURL1);
-      // Log the resulting object
-      console.log(response);
-      // Convert the temp K to fahrenheit
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-      // add temp content to html
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
       $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-      // Transfer content to HTML
       $(".city").html("<h1>" + response.name + " Weather Details</h1>");
       $(".tempF").text("Temperature: " + tempF.toFixed(2) + " °F");
       $(".humidity").text("Humidity: " + response.main.humidity + "%");
-      $(".wind").text("Wind Speed: " + (response.wind.speed*2.236936).toFixed(1) + " MPH"); //converts m/sec to mph
-      //getting lat and long for UV
+      $(".wind").text("Wind speed: " + (response.wind.speed*2.236936).toFixed(1) + " MPH"); //m per sec to mph
       var lon = response.coord.lon;
       var lat = response.coord.lat;
       var queryURL3 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
@@ -55,7 +35,6 @@ function renderWeather(){
         method: "GET"
       })
       .then(function(response){
-          console.log("here is the UV object " + queryURL3)
           var uvIndex = response.value;
           $("#UV").text(uvIndex);
           setUVColor();
@@ -71,60 +50,28 @@ function renderWeather(){
             }
           }
       });
-      document.querySelector(".citybox").innerHTML = response.name;
       document.querySelector("#bigcity").innerHTML = response.name + " ";
       var mainIcon = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png';
-      console.log("here's the icon link " + mainIcon);
       $("#mainIcon").empty();
       $("#mainIcon").append("<img src=" + mainIcon + ">");
-
-      
-      newHistory();
-
-      function newHistory() {
-        $("#searchicon").click(function(){
-          console.log("clicker")
-          var newBtn = document.createElement("BUTTON")
-          newBtn.setAttribute('class', 'citybox');
-          newBtn.innerHTML = response.name;
-          var cityList = document.getElementById("citylist");
-          cityList.appendChild(newBtn);
-        })
-      }
-
-      //from here, start putting each city search into local storage
-      //use a for loop to plug them into #last1, #last2, etc.
-      //have the #last1, #last2 retrieve cities from local storage with a delay of 1
-      //lastly, make each old city clickable
   });
-
-  //5 DAY FORECAST
-  // Here we run our AJAX call to the OpenWeatherMap API
   //for these, each response.list item goes +3 hours, hence 5, 13, 21, 29, 37
   $.ajax({
     url: queryURL2,
     method: "GET"
   })
-  // We store all of the retrieved data inside of an object called "response"
   .then(function(response) {
-    // Log the queryURL
-    console.log("forecast weather: " + queryURL2);
-    // Log the resulting object
-    console.log(response);
-    //pull temp & convert from K to F
-
     function pullTemp(listIndex) {
       tempF = (response.list[listIndex].main.temp - 273.15) *1.80 + 32;
       return tempF
     }
     //format date into mm/dd/yy. substr is (start, length)
-    //values for listIndex are given by when we run PullDate at the "add forecast date to html" sectoin
+    //values for listIndex are given by when we run PullDate at the "add forecast date to html" section
     function pullDate(listIndex) {
       var dateFlipper = response.list[listIndex].dt_txt;
       var mm = dateFlipper.substr(5, 2);
       var dd = dateFlipper.substr(8, 2);
       removeZero();
-
       function removeZero() {
         var digitOne = dd.charAt(0);
         var digitTwo = dd.charAt(1);
@@ -137,32 +84,22 @@ function renderWeather(){
       var yyyy = dateFlipper.substr(0, 4);
       return mm+"/"+removeZero()+"/"+yyyy;
     }
-    // add forecast date to html
     for(i = 0; i < 5; i++) {
       var dateid = "#fc-dt" + (i + 1);
       $(dateid).text(pullDate(5+8*i));
     }
-    //add forecast temps to html. 
-    // `pullTemp(5+8*i)` gets temps for all next 5 days. remember, response.list[] begins at [5]
-    // `"#fc-temp" + (i + 1)` runs through all 5 HTML temp divs
-    //toFixed(2) trims it to 2 decimal places
+    //`pullTemp(5+8*i)` gets temps for all next 5 days. remember, response.list[] begins at [5]
     for(i = 0; i < 5; i++) {
-      var dateid = "#fc-temp" + (i + 1);
+      var dateid = "#fc-temp" + (i + 1); //runs through all 5 HTML temp divs
       var smallIcon = 'https://openweathermap.org/img/wn/' + response.list[5+8*i].weather[0].icon + '.png';
-      console.log("here's the lil icon " + smallIcon)
-      $(dateid).text("Temp: " + pullTemp(5+8*i).toFixed(2) + " °F");
-      console.log("#smallIcon" + (i + 1));
+      $(dateid).text("Temp: " + pullTemp(5+8*i).toFixed(2) + " °F"); //toFixed(2) makes it 2 decimal places
+      $("#smallIcon" + (i + 1)).empty();
       $("#smallIcon" + (i + 1)).prepend("<img src=" + smallIcon + ">");
     } 
     //add forecast humidity to html
-    //`"#fc-humid" + (i + 1)` runs through all 5 HTML humidity divs
     for(i = 0; i < 5; i++) {
-      var dateid = "#fc-humid" + (i + 1);
+      var dateid = "#fc-humid" + (i + 1); //runs through all 5 HTML humidity divs
       $(dateid).text("Humidity: " + response.list[5+8*i].main.humidity + "%");
     }
   });
 }
-
-
-// use this for weather icon images
-// var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
